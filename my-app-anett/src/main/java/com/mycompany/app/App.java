5d415e4487447d9df39d46d755f88b3dda88306f
witchpoutraining <anett.huebner@starwit.de>
2023-09-04 16:11:14 +0200

    added Json

--- my-app-anett/src/main/java/com/mycompany/app/App.java
+++ my-app-anett/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        DemoFileReader fileReader = new DemoFileReader();
+        WelcomeDto welcome = fileReader.readFile("/welcome.json");
+        System.out.println(welcome.getMessage() + " " + welcome.getName());
     }
 }
